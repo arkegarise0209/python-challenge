9d4d03321407d859e9eaf88c3bb368fac08f98e6
Kegarise <kegarise@queensgranthigh.org>
2018-03-08 21:53:47 -0500

    Aesthetics

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -77,7 +77,7 @@ with open(file, newline="") as employee_info:
     for row in csvreader:
 
     #Add each employee ID to emp_ID list
-        emp_ID.append(row[0])
+        emp_ID.append((row[0]))
 
     #Add first name to first_name list
         first_name.append(row[1].split(" ")[0])
@@ -95,15 +95,15 @@ with open(file, newline="") as employee_info:
         state.append(us_state_abbrev[row[4]])
 
 #Zip lists together to make one dataset 
-clean_employee_records = list(zip(emp_ID,first_name,last_name,dob,ssn,state))
+clean_employee_records = zip(emp_ID,first_name,last_name,dob,ssn,state)
 
 #Output new file
 clean_data_file = os.path.join("employee_info.csv")
 
 with open(clean_data_file, "w" ,newline="") as csvfile:
-    clean_csv = csv.writer(csvfile, delimiter="\n")
-    clean_csv.writerow(["Emp ID,First Name,Last Name,DOB,SSN,State"])
-    clean_csv.writerow(str(clean_employee_records))
+    clean_csv = csv.writer(csvfile, delimiter=",")
+    clean_csv.writerow(["Emp ID","First Name","Last Name","DOB","SSN","State"])
+    clean_csv.writerows(clean_employee_records)
 
 
 
